fe03e03dd45b9d44a0d14c04e84517cb5c703beb
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-06-27 03:55:02 +0700

    On "Test_connection" selected logic block

--- app/src/main/java/com/example/wattsaver/MainActivity.kt
+++ app/src/main/java/com/example/wattsaver/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.wattsaver
 
 import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,10 +16,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        addBTDevicesToSpinner()
+
+        devicesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                if (selectedItem == "Test_connection") {
+                    Toast.makeText(applicationContext, "Connected successfully!", Toast.LENGTH_SHORT).show()
+
+
+                }
+            }
+
+        }
+    }
+
+    private fun addBTDevicesToSpinner() {
         val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         val pairedDevices = mBluetoothAdapter.bondedDevices
 
         val s: MutableList<String> = ArrayList()
+        s.add("<-Select device->")
+        s.add("Test_connection")
         for (bt in pairedDevices) s.add(bt.name)
 
         devicesSpinner.adapter = ArrayAdapter(this, R.layout.list_item,
