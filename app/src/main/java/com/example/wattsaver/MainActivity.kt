b3ebbcc779c0c1206a041a71e1fc7d30b313664d
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-06-27 03:06:21 +0700

    Bluetooth devices dropdown list

--- app/src/main/java/com/example/wattsaver/MainActivity.kt
+++ app/src/main/java/com/example/wattsaver/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.wattsaver
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices = mBluetoothAdapter.bondedDevices
+
+        val s: MutableList<String> = ArrayList()
+        for (bt in pairedDevices) s.add(bt.name)
+
+        devicesSpinner.adapter = ArrayAdapter(this, R.layout.list_item,
+            R.id.deviceNameTextView, s)
     }
 }
